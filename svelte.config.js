af1248f69ed9c02f0e7abc371ceb53206fac6092
FoxyGDFD <mic.panfil0v@gmail.com>
2025-12-05 12:04:25 +0300

    remove unused aliases

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,6 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			$: path.resolve('./src'),
-			$app: path.resolve('./src/app'),
-			$pages: path.resolve('./src/pages'),
 			$feature: path.resolve('./src/feature'),
 			$entities: path.resolve('./src/entities'),
 			$shared: path.resolve('./src/shared'),
