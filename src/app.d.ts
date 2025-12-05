9e8a429391320c971cc68a3e939a68164968db22
FoxyGDFD <mic.panfil0v@gmail.com>
2025-12-05 17:58:39 +0300

    fix vs code type declaration of .svelte declarations

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="@sveltejs/kit" />
 
-declare module '*.svelte' {
-	import type { SvelteComponentTyped } from 'svelte';
-	export default class Component extends SvelteComponentTyped<any, any, any> {}
-}
-
-declare module '*.svelte';
-
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -19,4 +12,4 @@ declare global {
 	}
 }
 
-export {};
+declare module '*.svelte' {}
